3601f90a41ce5c0ad4d8dc121d41820d331917a3
Kevin-Leguiza-Gaggero <kleguiza@jemersoft.com>
2023-12-22 09:40:52 -0300

    refactor: move 'product-card' into 'cards' category

--- src/components/Cards/index.ts
+++ src/components/Cards/index.ts
@@ -1,1 +1,2 @@
 export * from './Category-card/CategoryCard';
+export * from './Product-card/ProductCard';
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export * from './Carousels';
 export * from './Footers';
 export * from './Layout';
 export * from './Modals';
-export * from './Product-cards';
