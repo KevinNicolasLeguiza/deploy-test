221d43d51bdbd37ce797b3c9bdda491694b14544
Kevin-Leguiza-Gaggero <kleguiza@jemersoft.com>
2023-12-26 17:09:31 -0300

    feat: github action workflowOF

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000
-  },
+  base: '/deploy-test/',
   plugins: [react()],
   resolve: {
     alias: [
       {
         find: '@',
-        replacement: path.resolve(__dirname, "src")
-      }
-    ]
-  }
-})
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+});
